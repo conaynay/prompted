5009947a3b2ca82a8cda2876d063af5540f36fe2
conaynay <calcon007@gmail.com>
2018-04-22 16:24:09 -0700

    added models. start flex grid on html

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 'use strict';
 
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const { UserProfile, GameHistory, UserHistory, Games } = require('./models');
 
 app.use(express.static('public'));
+
+
 
 if(require.main == module){
   app.listen(process.env.PORT || 8080, function () {
--- test/test-server.js
+++ test/test-server.js
@@ -2,11 +2,47 @@
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const faker = require('faker');
 const app = require('../server.js');
 
 const expect = chai.expect;
 
 chai.use(chaiHttp);
+
+function generateUserProfileData() {
+  return {
+    'user_id': faker.random.number(),
+    'username': faker.internet.userName(),
+    'avatar': faker.image.imageUrl(),
+    'games': faker.random.number(),
+    'texts': faker.random.number(),
+    'achievements': [1,3],
+    'bio': faker.lorem.text(),
+    'created': Date.now()
+  };
+};
+
+function generateGameHistoryData() {
+  return {
+    'game_id': faker.random.number(),
+    'game': faker.random.number(),
+    'time_format': '30_min',
+    'timestamp': Date.now(),
+    'player': 2,
+    'move': 4,
+    'text': faker.lorem.text(),
+    'active': random.boolean()
+  };
+};
+
+function generateUserHistoryData() {
+  return {
+    'user_id': faker.random.number(),
+    'game_id': faker.random.number(),
+    'game': faker.random.number(),
+    'player': faker.random.number()
+  };
+};
 
 describe('index page', function(){
   it('should display index.html', function(){
@@ -17,4 +53,3 @@ describe('index page', function(){
       });
   });
 });
- 
